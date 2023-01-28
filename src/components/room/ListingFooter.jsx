800bbab6df49c8f6e4d8036be84094532914a12d
codedcontent <ogemephors002@gmail.com>
2023-01-28 10:17:10 +0100

    room footer breadcrumbs

--- src/components/room/ListingFooter.jsx
+++ src/components/room/ListingFooter.jsx
@@ -17,20 +17,39 @@ const otherOptions = [
 
 const ListingFooter = () => {
   return (
-    <div className="w-full bg-[#F7F7F7] px-52 py-4">
-      <div className="w-full">
+    <div className="w-full bg-[#F7F7F7] border-y-[1px]">
+      <div className="w-full px-52">
         {/* Other options */}
-        <div className="w-full">
+        <div className="w-full py-10 gap-6">
           <p className="font-medium text-xl">
             Explore other options in and around Ponta Delgada
           </p>
 
           <div className="grid grid-cols-4 w-full gap-4">
             {otherOptions.map((otherOption, index) => (
-              <p key={index} className="text-[#717171] w-max font-light">
+              <p
+                key={index}
+                className="text-[#717171] w-max font-light text-sm"
+              >
                 {otherOption}
               </p>
             ))}
+          </div>
+
+          <div className="mt-10 w-max flex gap-2 justify-center items-center">
+            <p className="text-sm font-extralight">Airbnb</p>
+
+            <i className="fa-solid fa-chevron-right fa-xs"></i>
+
+            <p className="text-sm font-extralight">Portugal</p>
+
+            <i className="fa-solid fa-chevron-right fa-xs"></i>
+
+            <p className="text-sm font-extralight">Azeros</p>
+
+            <i className="fa-solid fa-chevron-right fa-xs"></i>
+
+            <p className="text-sm font-extralight">São Miguel Island</p>
           </div>
         </div>
       </div>
